6efb3155ec329aa79b91c33c0ba0a386afe7c84f
Youngwook <youngwook@kookmin.ac.kr>
2024-09-08 10:27:49 +0900

    very slightly modify

--- step_coco/eval_cam_coco.py
+++ step_coco/eval_cam_coco.py
@@ -14,7 +14,8 @@ def run(args):
     n_images = 0
 
     for i, id in enumerate(ids):
-        label = np.array(Image.open('/data/coco_2014/coco_seg_anno/%s.png' % id))
+        # label = np.array(Image.open('/data/coco_2014/coco_seg_anno/%s.png' % id))
+        label = np.array(Image.open('../wsml2022/data/coco/mask/train2014/%s.png' % id.lstrip('0')))
         n_images += 1
         cam_dict = np.load(os.path.join(args.cam_out_dir, id + '.npy'), allow_pickle=True).item()
         if not ('high_res' in cam_dict):
